14b557b0d32d3d4e0878b9dda21182d08a65e090
Satyajitkumar123 <satyajitkumar2472@gmail.com>
2025-12-01 15:29:47 +0000

    Added Views and URL configuration

---
+++ jobhunter/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
+    # App
+    path('', views.job_list, name='job_list'),
+    path('add/', views.job_create, name='job_create'),
+    path('update/<int:pk>/', views.job_update, name='job_update'),
+    path('delete/<int:pk>/', views.job_delete, name='job_delete'),
+]
--- jobhunter/views.py
+++ jobhunter/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from .models import JobApplication
+from .forms import JobForm
 
-# Create your views here.
+
+@login_required
+def job_list(request):
+    jobs = JobApplication.objects.filter(user=request.user)
+    
+    query = request.GET.get('q')
+    if query:
+        jobs = jobs.filter(
+            Q(company__icontains=query) | Q(position__icontains=query)
+        )
+    return render(request, 'job_list.html', {'jobs': jobs})
+
+@login_required
+def job_create(request):
+    if request.method == 'POST':
+        form = JobForm(request.POST)
+        if form.is_valid():
+            job = form.save(commit=False)
+            job.user = request.user
+            job.save()
+            return redirect('job_list')
+    else:
+        form = JobForm()
+    return render(request, 'job_form.html', {'form': form, 'title': 'Add Job'})
+
+@login_required
+def job_update(request, pk):
+    job = get_object_or_404(JobApplication, pk=pk, user=request.user)
+
+    if request.method == 'POST':
+        form = JobForm(request.POST, instance=job)
+        if form.is_valid():
+            form.save()
+            return redirect('job_list')
+    else:
+        form = JobForm(instance=job)
+    return render(request, 'job_form.html', {'form': form, 'title': 'Edit Job'})
+
+@login_required
+def job_delete(request, pk):
+    job = get_object_or_404(JobApplication, pk=pk, user=request.user)
+    
+    if request.method == 'POST':
+        job.delete()
+        return redirect('job_list')
+    return render(request, 'job_confirm_delete.html', {'job': job})
